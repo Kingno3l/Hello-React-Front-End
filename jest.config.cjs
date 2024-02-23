ad3f74a98f669256b5466cc54d297ebca382c13a
kingno3l <nuelluiz@gmail.com>
2024-02-23 13:54:50 +0100

    unknown

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  testEnvironment: 'jsdom',
-};
