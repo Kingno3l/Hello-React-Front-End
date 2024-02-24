9f2124d96422a4215e158a3cbc9b387f2312e00f
kingno3l <nuelluiz@gmail.com>
2024-02-24 01:51:10 +0100

    fixed linters

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Greeting from './components/Greeting';
-import { fetchGreeting } from './redux/slices/greeting/greetingSlice';
 
 // Define your App component
 function App() {
